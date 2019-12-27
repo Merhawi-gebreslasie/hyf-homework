d08a6879d36c6740911e01977267aab334f21c50
Merhawi-gebreslasie <50158709+Merhawi-gebreslasie@users.noreply.github.com>
2019-12-27 02:35:56 +0100

    Changes for the hayBay

--- javascript/javascript2/week1/homeworkOne/hyf-Bay/main.js
+++ javascript/javascript2/week1/homeworkOne/hyf-Bay/main.js
@@ -1,85 +1,52 @@
-
-console.log('Script loaded');
-const testProductNames = ['Flat screen', 'Mobile phone', 'Wallet'];
-const ul = document.getElementById('list');
-// function renderProducts(products) {
-//     for (let i = 0; i < products.length; i++) {
-//         const produtName = products[i];
-//         const li = document.createElement('li');
-//         li.innerHTML = produtName;
-//         ul.appendChild(li);
+// testingfuntion 
+// console.log('Script loaded');
+// const testProdctNames = ['Flat screen', 'Mobile phone', 'Wallet'];
+// function testFunctionNames(products){
+//     for(let i=0;testProdctNames.length;i++){
+//         const testproduct=testProdctNames[i];
+//         const productUL=document.querySelector('section.products ul');
+//         const productLi=document.createElement('li');
+//         productLi.innerText=testproduct;
+//         productUL.appendChild(productLi);
 //     }
-
-
-
 // }
-// renderProducts(testProductNames);
+// testFunctionNames(testProdctNames);
 
+const productUL = document.querySelector('.products ul');
 
-let products2 = [{
-    id: 23771823,
-    name: 'Flat screen',
-    price: 4000,
-    rating: 4.2,
-    shipsTo: ['denmark', 'germany'],
-},
-{
-    id: 23555555,
-    name: 'Mobile phone',
-    price: 2000,
-    rating: 3.8,
-    shipsTo: ['norway', 'sweden'],
-},
-{
-    id: 23988080,
-    name: 'Wallet',
-    price: 500,
-    rating: 5.1,
-    shipsTo: ['germen', 'iceland'],
+function appendingLi(label, rowUL) {
+    const nameLI = document.createElement('li');
+    nameLI.innerText = label;
+    rowUL.appendChild(nameLI);
+
 }
-];
-// // showing more details to the user
-// function getAvailableProducts() {
+function renderProducts(products) {
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
 
-//     for (let i = 0; i < products2.length; i++) {
-//         let product = products2[i];
-//         const li = document.createElement('li');
-//         li.innerHTML = product.name + '|' + product.price + '|' + 
-//         product.rating + '|' +product.shipsTo;        }
-//         ul.appendChild(li);
+        const rowLI = document.createElement('li');
+        const rowUL = document.createElement('ul')
+        productUL.appendChild(rowLI);
+        rowLI.appendChild(rowUL);
 
-//     }
-//     return products2;
-// }
-// console.log(getAvailableProducts());
-function getAvailableProducts(){
-    for(let i=0; i<products2.length;i++){
-        let product = products2[i];
-                const li = document.createElement('li');
-                li.innerHTML = product.name + '|' + product.price + '|' + 
-                product.rating ;
-                const innerUl=document.createElement('ul');
-                for(let j=0;j<product.shipsTo.length;j++){
-                    const ship_to=product.shipsTo[j];
-                   
-                    const innerList=document.createElement('li');
-                    
-                    
-                    console.log('here'+ship_to);
-                    
-                           innerList.innerHTML=ship_to;
-                           li.appendChild(innerUl);
-                        
-                           innerUl.appendChild(innerList);
-                           
-                    
-                }
-                ul.appendChild(li);
-                   
-                   
-                
-                }
-                return products2;
-            }
-            console.log(getAvailableProducts());
+        appendingLi(product.name, rowUL);
+        appendingLi(product.price, rowUL);
+        appendingLi(product.rating, rowUL);
+        appendingLi(product.id, rowUL);
+        appendingLi(product.shipsTo, rowUL);
+
+        const shipsToRowLi = document.createElement('li');
+        rowUL.appendChild(shipsToRowLi);
+
+        const shipsToRowUL = document.createElement('ul')
+        shipsToRowLi.appendChild(shipsToRowUL);
+        product.shipsTo.forEach(country => {
+            appendingLi(country, shipsToRowUL);
+        });
+
+    }
+
+}
+const products = getAvailableProducts();
+renderProducts(products);
 
