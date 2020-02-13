32719fc7035c672bdefe27fd8ee8a1d7a816a5d1
Merhawi-gebreslasie <50158709+Merhawi-gebreslasie@users.noreply.github.com>
2020-02-13 11:58:17 +0100

    react/week1

---
+++ react/week1/src%public/src/components/TodoList.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { TodoItem } from './TodoItem';
+
+
+// functional Component
+
+export const TodoList=({todos})=>{
+    return(
+<div className='lists'>
+<ul>
+
+{todos.map((todo,index)=>(
+    <TodoItem 
+    key={index}
+    description={todo.description}
+    dueDate={todo.dueDate}
+    />
+))}
+
+</ul>
+
+</div>
+
+
+    )
+};
+
+// Class Component
+// class TodoList extends Component{
+// render(){
+//         return(
+//             <div className='lists'>
+// {this.todos.map((todo,index)=>(
+//     <TodoItem 
+//     key={index}
+//     description={todo.description}
+//     dueDate={todo.dueDate}
+//     />
+// ))}
+
+
+// </div>
+
+
+//         )
+//     }
+// }
+// export default TodoList;
