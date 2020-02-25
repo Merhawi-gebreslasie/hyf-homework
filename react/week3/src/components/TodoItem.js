06e7b9962c7c97b55e1a5f5b8b0bc2a1fda12f7e
Merhawi-gebreslasie <50158709+Merhawi-gebreslasie@users.noreply.github.com>
2020-02-25 10:57:23 +0100

    add edit functionality

---
+++ react/week3/src/components/TodoItem.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+// import { Border } from "./Border";
+
+export const TodoItem = (props) => {
+    // Destracturing assignmentExpression
+    const { description, deadline, handleDelet, handleEdit } = props;
+
+    const [isDone, setIsDone] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
+
+    const handleChange = () => {
+        setIsDone(!isDone);
+    };
+
+    const onEdit = () => {
+        setIsEdit(!isEdit);
+    };
+    return (
+        <div className="items">
+
+        
+        <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+        {isEdit ? (
+            <input onChange={handleEdit} value={description} />
+        ) : (
+                <span>
+                    {description}{''} |{' '} {deadline}
+                </span>
+            )}
+    </li>
+    <li>
+        <input onChange={handleChange} type="checkbox" />
+    </li>
+
+    <li>
+        <button className="delete" onClick={handleDelet}>
+            Delete
+</button>
+
+        <button onClick={onEdit}>
+            <span>{!isEdit ? <span>Edit </span> : <span>update</span>}</span>
+        </button>
+    </li>
+ 
+        </div>
+    );
+}
+
+// TodoItem.propTypes={
+//     description:PropTypes.string,
+//     deadline:PropTypes.instanceOf(Date),
+//     handleDelet:PropTypes.func.isRequired,
+//     handleEdit:PropTypes.func.isRequired,
+//     isDone:PropTypes.bool
+// }
