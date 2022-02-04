b9679a85c7594c60f81e6da6c290985cd9876efe
Merhawi-gebreslasie <50158709+Merhawi-gebreslasie@users.noreply.github.com>
2022-02-05 00:16:27 +0100

    Update main.js
    modify nameing and handle error

--- javascript/javascript3/week3/homework/promise/main.js
+++ javascript/javascript3/week3/homework/promise/main.js
@@ -1,12 +1,13 @@
 const div=document.createElement('div')
 async function fecthData(){
-  const response1=await  fetch('https://api.github.com/search/repositories?q=user:Vahablotfi')
-  const response2=await  fetch('https://api.github.com/search/repositories?q=user:mariem460')
-  const response3=await  fetch('https://api.github.com/search/repositories?q=user:asmafassy')
+  try{
+  const vahabUser=await  fetch('https://api.github.com/search/repositories?q=user:Vahablotfi')
+  const mariemUser=await  fetch('https://api.github.com/search/repositories?q=user:mariem460')
+  const asmaUser=await  fetch('https://api.github.com/search/repositories?q=user:asmafassy')
 
-  const user1=await response1.json()
-  const user2=await response2.json()
-  const user3=await response3.json()
+  const user1=await vahabUser.json()
+  const user2=await mariemUser.json()
+  const user3=await asmaUser.json()
 const results= await Promise.all([user1,user2,user3])
  const result= results.map(item=>'<ul>'+
  '<li>'+'Full name :' +item.items[0].full_name+'</li>'+
@@ -16,5 +17,7 @@ const results= await Promise.all([user1,user2,user3])
 );
 div.innerHTML=result;
 document.body.appendChild(div)
+  } catch(err){
+    console.error(err);
 }
 fecthData()
