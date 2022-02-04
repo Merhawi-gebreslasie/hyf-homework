e13adb153a07202593a86298f282d9d2e528519d
Merhawi-gebreslasie <50158709+Merhawi-gebreslasie@users.noreply.github.com>
2022-02-05 00:31:44 +0100

    Update main.js

--- javascript/javascript3/week3/homework/promise/main.js
+++ javascript/javascript3/week3/homework/promise/main.js
@@ -1,15 +1,13 @@
 const div=document.createElement('div')
 async function fecthData(){
   try{
-  const vahabUser=await  fetch('https://api.github.com/search/repositories?q=user:Vahablotfi')
-  const mariemUser=await  fetch('https://api.github.com/search/repositories?q=user:mariem460')
-  const asmaUser=await  fetch('https://api.github.com/search/repositories?q=user:asmafassy')
+  const vahabUser=await  fetch('https://api.github.com/search/repositories?q=user:Vahablotfi').then(user=>user.json());
+  const mariemUser=await  fetch('https://api.github.com/search/repositories?q=user:mariem460').then(user=>user.json());
+  const asmaUser=await  fetch('https://api.github.com/search/repositories?q=user:asmafassy').then(user=>user.json());
 
-  const user1=await vahabUser.json()
-  const user2=await mariemUser.json()
-  const user3=await asmaUser.json()
-const results= await Promise.all([user1,user2,user3])
- const result= results.map(item=>'<ul>'+
+ 
+const users= await Promise.all([vahabUser,mariemUser,asmaUser])
+ const result= users.map(item=>'<ul>'+
  '<li>'+'Full name :' +item.items[0].full_name+'</li>'+
  '<li>' +'Url :' +item.items[0].url+'</li>'+
  '<li>' +'Owner :'+item.items[0].html_url+'</li>'+
