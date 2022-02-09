d02eb33fb9f2890d4a2f5b61c55e0b5cb3bbda95
Merhawi-gebreslasie <50158709+Merhawi-gebreslasie@users.noreply.github.com>
2022-02-09 21:45:48 +0100

    Update hyf_lesson1_homework_queries.sql

--- databases/week1/hyf_lesson1_homework_queries.sql
+++ databases/week1/hyf_lesson1_homework_queries.sql
@@ -1,25 +1,27 @@
 USE hyf_lesson1;
---1
+-- 1
 SELECT COUNT(*) AS 'count task '
  FROM task;
- --2
+ -- 2
 SELECT  COUNT(*) AS 'count task '
  FROM task
  WHERE due_date IS NULL;
- --3
- SELECT * FROM task t JOIN status s ON t.status_id=s.id
+ -- 3
+ SELECT * FROM task t 
+ JOIN status s ON t.status_id=s.id
  WHERE s.name ='Done';
- --4
- SELECT * FROM task t JOIN status s ON t.status_id=s.id
+ -- 4
+ SELECT * FROM task t 
+ JOIN status s ON t.status_id=s.id
  WHERE s.name !='Done';
- --5
+ -- 5
  SELECT * FROM task 
  ORDER BY due_date DESC;
- --6
+ -- 6
  SELECT * FROM task 
  ORDER BY due_date DESC
  LIMIT 1;
- --7
+ -- 7
  SELECT title,due_date 
  FROM task 
  WHERE title REGEXP 'database'
@@ -27,13 +29,16 @@ SELECT  COUNT(*) AS 'count task '
 --8
 
 SELECT task.title,status.name AS text 
-FROM task JOIN status; 
---9
-SELECT status.name,COUNT(*) AS frequency 
-FROM task JOIN status ON task.status_id=status.id
+FROM task 
+JOIN status; 
+-- 9
+SELECT status.name,COUNT(id) AS frequency 
+FROM task 
+JOIN status ON task.status_id=status.id
 GROUP BY status.name;
---10
+-- 10
 SELECT status.name,COUNT(*) AS frequency 
-FROM task JOIN status ON task.status_id=status.id
+FROM task 
+JOIN status ON task.status_id=status.id
 GROUP BY status.name
 ORDER BY frequency DESC;
