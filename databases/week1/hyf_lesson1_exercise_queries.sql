ee1e423b7d8a21edd12843df3bf893716cb46226
Merhawi-gebreslasie <50158709+Merhawi-gebreslasie@users.noreply.github.com>
2022-02-09 21:41:20 +0100

    Update hyf_lesson1_exercise_queries.sql

--- databases/week1/hyf_lesson1_exercise_queries.sql
+++ databases/week1/hyf_lesson1_exercise_queries.sql
@@ -1,36 +1,39 @@
 
 USE hyf_lesson1;
---1
+-- 1
 SELECT name,phone FROM user;
---2
-SELECT name FROM user WHERE id=10
+-- 2
+SELECT name FROM user 
+WHERE id=10
 ;
---3
+-- 3
 SELECT COUNT(*) FROM user;
---4
-SELECT name from user
+-- 4
+SELECT name FROM user
 LIMIT 5;
---5
-SELECT name from user
+-- 5
+SELECT name FROM user
 LIMIT 9,3;
---6
+-- 6
 SELECT SUM(id) FROM user;
---7
+-- 7
 SELECT * FROM user
 ORDER BY name ;
---8
+-- 8
 SELECT * FROM task
 WHERE description like  '%SQL%'
 OR title LIKE '%SQL%';
---9
+-- 9
 SELECT task.title FROM user 
-JOIN task ON task.user_id=user.id AND user.name REGEXP 'Maryrose';
---10
-SELECT user.name, COUNT(*) AS count 
-FROM user JOIN task ON   task.user_id=user.id
+JOIN task ON task.user_id=user.id 
+   AND user.name REGEXP 'Maryrose';
+-- 10
+SELECT user.name, COUNT(id) AS count 
+FROM user 
+JOIN task ON   task.user_id=user.id
 GROUP BY user.name;
---11
-SELECT user.name, COUNT(*) AS count_done 
+-- 11
+SELECT user.name, COUNT(id) AS count_done 
 FROM user 
 JOIN task ON   task.user_id=user.id
 JOIN status ON task.status_id=status.id
